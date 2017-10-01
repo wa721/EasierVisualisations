b89449eb7f2856ea90182c17c66cfa4da3cb8f0e
William Ashford <willjashford@gmail.com>
2017-10-01 13:50:28 +0100

    Update easyMaps.js
    Rectifies issues with non-global functions

--- easyMaps.js
+++ easyMaps.js
@@ -5,35 +5,7 @@ window.MakeMapOnLoad = function(mapFunction){
 window.HeatMap = function(inputData){
   function toggleHeatmap() {
       heatmap.setMap(heatmap.getMap() ? null : map);
-    }
-
-  function changeGradient() {
-    var gradient = [
-      'rgba(0, 255, 255, 0)',
-      'rgba(0, 255, 255, 1)',
-      'rgba(0, 191, 255, 1)',
-      'rgba(0, 127, 255, 1)',
-      'rgba(0, 63, 255, 1)',
-      'rgba(0, 0, 255, 1)',
-      'rgba(0, 0, 223, 1)',
-      'rgba(0, 0, 191, 1)',
-      'rgba(0, 0, 159, 1)',
-      'rgba(0, 0, 127, 1)',
-      'rgba(63, 0, 91, 1)',
-      'rgba(127, 0, 63, 1)',
-      'rgba(191, 0, 31, 1)',
-      'rgba(255, 0, 0, 1)'
-    ]
-    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
   }
-
-  function changeRadius() {
-      heatmap.set('radius', heatmap.get('radius') ? null : 20);
-    }
-
-  function changeOpacity() {
-      heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
-    }
 
   var mapOptions = {
       center: new google.maps.LatLng(36.2021, 37.1343),
@@ -57,3 +29,29 @@ window.HeatMap = function(inputData){
       map: map
     });
 }
+
+window.changeGradient = function() {
+    var gradient = [
+      'rgba(0, 255, 255, 0)',
+      'rgba(0, 255, 255, 1)',
+      'rgba(0, 191, 255, 1)',
+      'rgba(0, 127, 255, 1)',
+      'rgba(0, 63, 255, 1)',
+      'rgba(0, 0, 255, 1)',
+      'rgba(0, 0, 223, 1)',
+      'rgba(0, 0, 191, 1)',
+      'rgba(0, 0, 159, 1)',
+      'rgba(0, 0, 127, 1)',
+      'rgba(63, 0, 91, 1)',
+      'rgba(127, 0, 63, 1)',
+      'rgba(191, 0, 31, 1)',
+      'rgba(255, 0, 0, 1)'
+    ]
+    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+  }
+window.changeRadius = function() {
+      heatmap.set('radius', heatmap.get('radius') ? null : 20);
+    }
+window.changeOpacity = function() {
+      heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+    }
